90603371a959d98b6a04e30ff1fc6389c6b7f099
Tom Wilhelm <tomrwilhelm@gmail.com>
2020-12-06 10:01:43 -0800

    added right click handler

---
+++ Runtime/RightClickHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace TW.UI
+{
+	public class RightClickHandler : MonoBehaviour, IPointerClickHandler
+	{
+		public UnityPointerEvent onRightClick = new UnityPointerEvent();
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			if (eventData.button != PointerEventData.InputButton.Right)
+				return;
+
+			if (!enabled)
+				return;
+
+			onRightClick.Invoke(eventData);
+		}
+	}
+
+}
